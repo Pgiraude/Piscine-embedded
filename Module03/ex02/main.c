a760ca382969df39ac012897acec0f5949666c98
Pierre GIRAUDEAU <pgiraude@f1br7s9.clusters.42paris.fr>
2025-11-03 16:55:44 +0100

    ex02

--- Module03/ex02/main.c
+++ Module03/ex02/main.c
@@ -4,28 +4,47 @@
 #define F_CPU 16000000UL
 #define DUTY_CYCLE (F_CPU / (PRESCALER * 256))
 
-// void wheel(uint8_t pos) {
-//     pos = 255 - pos;
-//     if (pos < 85) {
-//         set_rgb(255 - pos * 3, 0, pos * 3);
-//     }
-//     else if (pos < 170) {
-//         pos = pos - 85;
-//         set_rgb(0, pos * 3, 255 - pos * 3);
-//     }
-//     else {
-//         set_rgb(pos * 3, 255 - pos * 3, 0);
-//     }
-// }
+void set_rgb(uint8_t r, uint8_t g, uint8_t b) {
+    OCR0A = r;
+    OCR0B = g;
+    OCR2B = b;
+}
+
+void wheel(uint8_t pos) {
+    pos = 255 - pos;
+    if (pos < 85) {
+        set_rgb(255 - pos * 3, 0, pos * 3);
+    } else if (pos < 170) {
+        pos = pos - 85;
+        set_rgb(0, pos * 3, 255 - pos * 3);
+    } else {
+        pos = pos - 170;
+        set_rgb(pos * 3, 255 - pos * 3, 0);
+    }
+}
 
 int main(void) {
     DDRD = (1 << PD3) | (1 << PD6) | (1 << PD5);
+
     TCCR0A |= (1 << WGM00) | (1 << WGM01); // Fast PWM mode
     TCCR0B |= (1 << CS02);                 // Prescaler = 256
-    TCCR0A |= (1 << COM0A1); // clear on compare match, set at BOTTOM so = 1
+    // clear on compare match, set at BOTTOM so = 1
+    TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
 
-    OCR0A = 0;
+    TCCR2A |= (1 << WGM20) | (1 << WGM21); // Fast PWM mode
+    TCCR2A |= (1 << COM2B1);
+    TCCR2B |= (1 << CS22) | (1 << CS21); // Prescaler = 256
+
+    int i = -1;
     while (1) {
+        while (++i < 255) {
+            wheel(i);
+            _delay_ms(10);
+        }
+        while (--i > 0) {
+            wheel(i);
+            _delay_ms(10);
+        }
     }
 
     return 0;
